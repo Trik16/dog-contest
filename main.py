966675887495c683a3945deb9356a7c68f928ca6
karim <karim.dimashki@konicaminolta.it>
2021-10-12 15:25:22 +0200

    Main code files structure

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Main code structrure
+"""
+
+import json
+from dataclasses import dataclass
+
+def main () -> None:
+
+    # read data from database
+    with open("./database/data.json") as file:
+        data = json.load(file)
